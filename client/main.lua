b014301dd23440360d90b5abf9d70a8cd6b6aca4
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-08-24 23:56:54 -0300

    optimize

--- client/main.lua
+++ client/main.lua
@@ -1,43 +1,14 @@
-require "ui"
-require "client"
-require "country"
+require "states"
+require "network"
 
-love.graphics.setBackgroundColor(0.28, 0.28, 0.28)
+love.graphics.setBackgroundColor(0.22, 0.22, 0.22)
 
-local currentState = 1
-local uis = {
-
-    [1] = userInterface(),
-}
-
-local gameStates = {
-
-    mainMenu = 1,
-}
-
-uis[gameStates.mainMenu]:addButton("Play", 50, 350, 200, 80, 80, nil, function()
-
-end)
-
-uis[gameStates.mainMenu]:addButton("Exit", 50, 500, 200, 80, 80, nil, function()
-
-    love.window.close()
-end)
-
-function love.mousepressed(x, y, button, istouch)
-
-    if button == 1 then
-
-        uis[currentState]:update(x, y)
-    end
-end
-
+setGameState("menu")
 function love.draw()
 
-    uis[currentState]:draw()
+    drawGame()
 end
 
 function love.update()
 
-    update()
 end
