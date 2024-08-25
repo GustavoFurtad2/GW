ea0394189ce6327342185b7a3c8d60f95339600f
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-08-24 23:57:11 -0300

    Create font.lua

---
+++ client/font.lua
@@ -0,0 +1,17 @@
+local fontSizes = {}
+local lastSize
+
+function setFontSize(size)
+
+    if lastSize == size then
+        return
+    end
+
+    if not fontSizes[size] then
+
+        fontSizes[size] = love.graphics.newFont("assets/arial.ttf", size)
+        love.graphics.setFont(fontSizes[size])
+    else
+        love.graphics.setFont(fontSizes[size])
+    end
+end
