fbe9994897e63eb490a52104f45a81b7991e211e
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-08-25 22:01:02 -0300

    fix mouse pressed doesn't work glitch

--- client/states/menu.lua
+++ client/states/menu.lua
@@ -3,7 +3,7 @@ require "font"
 
 local currentGameState = "menu"
 
-local menu = Gui()
+menu = Gui()
 local text = menu:textLabel("Geografic Wars", 80, 25, 25)
 
 function play()
@@ -13,14 +13,6 @@ end
 
 local play = menu:textButton("Play", 80, 25, 250, 200, 80, play)
 local exit = menu:textButton("Exit", 80, 25, 335, 200, 80, love.window.close)
-
-function love.mousepressed(x, y, button)
-
-    if button == 1 then
-
-        menu:button(x, y)
-    end
-end
 
 function drawMenu()
 
