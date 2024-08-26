d62417b5cc5ecc849514cd2a7873e61c1c93b5eb
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-08-25 22:00:30 -0300

    add Username input

--- client/states/login.lua
+++ client/states/login.lua
@@ -3,15 +3,50 @@ require "font"
 
 local currentGameState = "login"
 
-local login = Gui()
+login = Gui()
 
-local exit = login:textButton("Exit", 80, 25, 335, 200, 80, love.window.close)
+local isTyping = false
 
-function love.mousepressed(x, y, button)
+local username = login:textLabel("Username", 80, 420, 255)
 
-    if button == 1 then
+function isAlphanumeric(c)
 
-        login:button(x, y)
+    return c:upper() ~= c:lower() or tonumber(c) ~= nil
+end
+
+function typemode()
+
+    isTyping = not isTyping
+end
+
+function changeState()
+
+    currentGameState = "rooms"
+end
+
+local textBox = login:textButton("", 80, 320, 335, 600, 80, typemode)
+textBox.showBox = true
+
+local enter = login:textButton("Next", 60, 780, 420, 140, 60, changeState)
+enter.showBox = true
+enter.visible = false
+
+function type(key)
+
+    if isAlphanumeric(key) and key:len() == 1 then
+
+        textBox.text = textBox.text .. key
+    elseif key == "backspace" then
+        textBox.text = textBox.text:sub(1, textBox.text:len() - 1)
+    end
+
+    enter.visible = textBox.text:len() > 0
+end
+
+function love.keypressed(key, scancode)
+
+    if isTyping then
+        type(scancode)
     end
 end
 
