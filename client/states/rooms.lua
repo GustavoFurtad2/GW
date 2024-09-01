86f8e0838c412544bd22e0a357f62299413fe48e
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-09-01 10:36:10 -0300

    add room list

--- client/states/rooms.lua
+++ client/states/rooms.lua
@@ -1,13 +1,42 @@
 require "ui"
 require "font"
 
-rooms = Gui()
+rooms = {}
+roomsGui = Gui()
+
+function createRoom(roomName, roomID, owner, players, maxPlayers)
+
+    rooms[roomID] = {
+
+        owner = owner,
+        players = players,
+        maxPlayers = maxPlayers,
+
+        roomName = roomName,
+    }
+end
+
+function updateRoomStatus(roomID, players, maxPlayers)
+
+    rooms[roomID].players = players
+    rooms[roomID].maxPlayers = maxPlayers
+end
+
+function closeRoom(roomID)
+
+    table.remove(rooms, roomID)
+end
 
 local currentGameState = "rooms"
 
+for k, v in next, rooms do
+    roomsGui:textButton(v.roomName, 80, 50, 90 + (k - 1) * 90, 200, 80, play)
+end
+
 function drawRooms()
 
-    rooms:draw()
+    roomsGui:draw()
+
 
     return currentGameState
 end
