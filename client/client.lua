90faaafdf24629dcd8e8fc856cd1ea80ce9ae267
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-09-06 23:27:50 -0300

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,36 +1,6 @@
-local socket, json = require("socket"), require("json")
+require "env"
 
-local tcp = socket.tcp()
+function updateLogin(data)
 
-tcp:settimeout(0)
-tcp:connect("127.0.0.1", 8081)
-
-local receivedData = ""
-
-tcp:send("teste love")
-function update()
-
-    local data, err, partial = tcp:receive("*a")
-    if data or (partial and partial ~= "") then
-
-        if data then
-            processReceivedData(data)
-        elseif partial then
-            receivedData = receivedData .. partial
-            processReceivedData(receivedData)
-        end
-
-    elseif err ~= "timeout" then
-        print("Connection closed or error:", err)
-    end
-end
-
-function processReceivedData(data)
-
-    print("Received data: " .. data)
-end
-
-
-function love.quit()
-    tcp:close()
+    currentGameState = "rooms"
 end
