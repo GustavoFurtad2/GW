870aa45c94d36d3d8d991f7839ea7f10cf930cea
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-09-15 23:02:25 -0300

    room now is a class

--- server/rooms.js
+++ server/rooms.js
@@ -1,18 +1,33 @@
 let rooms = []
 
-function createRoom(roomName, roomHoster) {
+class Room {
 
-    rooms[rooms.length] = {
-        "roomName": roomName,
-        "roomHoster": roomHoster
+    open = true
+
+    constructor(roomName, roomHoster, players, maxPlayers) {
+
+        this.roomName = roomName
+        this.roomHoster = roomHoster
+        this.players = players
+        this.maxPlayers = maxPlayers
     }
 }
+
+function createRoom(roomName, roomHoster, maxPlayers) {
+
+    rooms[rooms.length] = new Room(roomName, roomHoster, 1, maxPlayers)
+}
+
+createRoom("tobas", "flavin do pneu", 8)
+createRoom("sala dos crias", "joazinhogameplays1337", 5)
+createRoom("Enigmas", "CellBit", 90)
+createRoom("L", "Kira", 11)
 
 function getRoomList() {
     return rooms
 }
 
 module.exports = {
-    createRoom,
+    Room,
     getRoomList
 }
